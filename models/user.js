a269aa4f19c8dea41a858a6eba1662b8bd67b112
MuhammadIB88 <dsledge88@gmail.com>
2026-01-17 14:47:30 +0100

    update: add city field to User model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,8 @@ const UserSchema = new mongoose.Schema({
   password: { type: String, required: true },
   country: { type: String, required: true },
   state: { type: String, required: true },
+  // Added city field to store the specific city for more accurate Azaan times
+  city: { type: String, default: null },
   // Adding fcmToken here. It is not 'required', so it won't break 
   // existing users when they log in.
   fcmToken: { type: String, default: null }
